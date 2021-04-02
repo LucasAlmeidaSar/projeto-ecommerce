afc1aab6858eadb279c6b4bba3add433903aab46
Lucas de Almeida Sarmento <lukas.barcee@gmail.com>
2021-04-02 17:31:36 -0300

    Ajustes no layout geral da Home, assim como nos arquivos css

--- modules/scroll.js
+++ modules/scroll.js
@@ -1,11 +1,11 @@
 import alterarClassesDosElementos from './util.js'
 
+
 // Elementos
 const menuTopo = document.querySelector('.nav-principal__menu-topo')
 const dropdownConteudo = document.querySelector('.dropdown-conteudo')
 const btnTopo = document.querySelectorAll('.btn-nav')
 const iconeTopo = document.querySelectorAll('.icone-nav')
-
 
 
 export default function ativarEfeitosDeScroll() {
