40bc5a94739c1a572e2221ff2673d4bed13c5494
Lucas de Almeida Sarmento <lukas.barcee@gmail.com>
2021-04-03 18:15:30 -0300

    Adicionado efeito de scroll no nav-celular e adicionado funcionalidade do botão do menu hamburguer, assim como efeitos de transições

--- modules/scroll.js
+++ modules/scroll.js
@@ -6,14 +6,16 @@ const menuTopo = document.querySelector('.nav-principal__menu-topo')
 const dropdownConteudo = document.querySelector('.dropdown-conteudo')
 const btnTopo = document.querySelectorAll('.btn-nav')
 const iconeTopo = document.querySelectorAll('.icone-nav')
+const navCelular = document.querySelector('.nav-principal__menu-topo-sm')
+const btnNacCelular = document.querySelector('.menu-topo-sm__btn')
 
 
 export default function ativarEfeitosDeScroll() {
     
     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-        alterarClassesDosElementos('adicionar', 'on-scroll', menuTopo, dropdownConteudo, btnTopo, iconeTopo)
+        alterarClassesDosElementos('adicionar', 'on-scroll', menuTopo, dropdownConteudo, btnTopo, iconeTopo, navCelular, btnNacCelular)
     }else{
-        alterarClassesDosElementos('remover', 'on-scroll', menuTopo, dropdownConteudo, btnTopo, iconeTopo) 
+        alterarClassesDosElementos('remover', 'on-scroll', menuTopo, dropdownConteudo, btnTopo, iconeTopo, navCelular, btnNacCelular) 
     }
 
 }
