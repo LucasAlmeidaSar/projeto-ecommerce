60a897b7d58ecc78e74869ccd4312c2ad6d666f2
Lucas de Almeida Sarmento <lukas.barcee@gmail.com>
2021-04-05 16:35:42 -0300

    Melhoria arquivo util.js

--- modules/util.js
+++ modules/util.js
@@ -2,43 +2,43 @@ const classesDosElementos = {
     adicionar(classe, ...elementos){
        for(const elemento of elementos){
            const verificador = elemento.length
-           verificador !== undefined ? varrerColecaoEncontrada('adicionar', classe, elemento) : elemento.classList.add(classe)           
+           verificador !== undefined ? varrerColecaoEncontrada.adicionar(classe, elemento) : elemento.classList.add(classe)           
        }
     },
 
     remover(classe, ...elementos){
        for (const elemento of elementos){
            const verificador = elemento.length
-           verificador !== undefined ? varrerColecaoEncontrada('remover', classe , elemento) : elemento.classList.remove(classe)
+           verificador !== undefined ? varrerColecaoEncontrada.remover(classe , elemento) : elemento.classList.remove(classe)
        }
     },
     
     alternar(classe, ...elementos){
        for (const elemento of elementos){
            const verificador = elemento.length
-           verificador !== undefined ? varrerColecaoEncontrada('alternar', classe , elemento) : elemento.classList.toggle(classe)
+           verificador !== undefined ? varrerColecaoEncontrada.alternar(classe , elemento) : elemento.classList.toggle(classe)
        }
     }
 }
 
- 
- function varrerColecaoEncontrada(operacao, classe, elementos){
-     if (operacao === 'adicionar'){
-         for (const elemento of elementos) {
-             elemento.classList.add(classe)
-         }
-     }
-     if (operacao === 'remover'){
-         for (const elemento of elementos) {
-             elemento.classList.remove(classe)
-         }
-     } 
-     if (operacao === 'alternar') {
-         for (const elemento of elementos){
+
+ const varrerColecaoEncontrada = {
+     adicionar(classe, elementos){
+        for (const elemento of elementos) {
+            elemento.classList.add(classe)
+        }
+     },
+     remover(classe, elementos){
+        for (const elemento of elementos) {
+            elemento.classList.remove(classe)
+        }
+     },
+     alternar(classe, elementos){
+        for (const elemento of elementos){
             elemento.classList.toggle(classe)
          }
      }
  }
-
+ 
 
  export default classesDosElementos
