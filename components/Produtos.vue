1c38131562c54bb099e05bbd71e0a7042731947b
Silas Fernandes da Cunha <silascunha09@hotmail.com>
2021-05-01 15:48:15 -0300

    Componentes iniciais da tela de CMS de admin

---
+++ components/Produtos.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- Estrutura da página aqui, é necessário manter essa div como pai dos outros elementos -->
+    <div class="exemplo">
+        <span>Tela de Produtos</span>
+    </div>
+</template>
+
+<script>
+//código JavaScript vem aqui, com a estrutura do Vue, não é necessário mexer
+module.exports = {
+
+}
+</script>
+
+<style scoped>
+/* estilização do componente aqui, é só colocar as classes nos elementos dentro da tag template */
+
+.exemplo {
+    font-size: 26px;
+    text-align: center;
+    padding: 32px;
+}
+</style>
